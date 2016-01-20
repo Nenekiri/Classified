cef05e2152da99884fe193cd2b79601af2a7388f
Nenekiri <MJM16@pct.edu>
2016-01-20 17:33:41 -0500

    first official commit for this project

---
+++ Assets/TestScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+    //This scripts purpose is going to be to test the way that the new git repository commits from the local system. 
+    //Hooray!
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
