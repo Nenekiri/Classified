8a32bc2dc5c72e86e77a18f98d926f881807e6ca
Nenekiri <MJM16@pct.edu>
2016-01-21 22:12:56 -0500

    started re-learning how to use dialoguer

---
+++ Assets/DialoguerGUI.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialoguerGUI : MonoBehaviour {
+
+    private bool showing;
+    private string text;
+    private string[] choices; 
+
+    // Use this for initialization
+    void Start() {
+        Dialoguer.events.onStarted += onStarted;
+        Dialoguer.events.onEnded += onEnded;
+        Dialoguer.events.onTextPhase += onTextPhase;
+
+    }
+
+    void OnGUI() {
+        if (!showing)
+            return;
+
+        GUI.Box(new Rect(10, 10, 200, 150), text);
+
+
+        if (choices == null)
+        {
+            if (GUI.Button(new Rect(10, 220, 200, 30), "keep going"))
+            {
+                Dialoguer.ContinueDialogue(0);
+            }
+        }
+        else {
+            for (int i = 0; i < choices.Length; i++) {
+                if (GUI.Button(new Rect(10, 220 + (40 * i), 200, 30), choices[i])){
+                    Dialoguer.ContinueDialogue(i); 
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void onStarted() {
+        showing = true; 
+    }
+
+    private void onEnded() {
+        showing = false; 
+    }
+
+    private void onTextPhase(DialoguerTextData data){
+         text = data.text; 
+        }
+
+}
--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -6,13 +6,48 @@ public class TestScript : MonoBehaviour {
     //This scripts purpose is going to be to test the way that the new git repository commits from the local system. 
     //Hooray!
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    //This script is also going to be used for testing out dialoguer options and styles
+
+    //start up the dialoguer stuff
+    void Awake() {
+        Dialoguer.Initialize();
+    }
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnGUI() {
+        if (GUI.Button(new Rect(10, 10, 100, 30), "Start!"))
+        {
+
+            // The preferred way to start dialogues is with the DialoguerDialogues enum
+            // Like so:
+            // Dialoguer.StartDialogue(DialoguerDialogues.My_First_Dialogue_Tree);
+
+            // Or you can start it by passing the ID of the dialogue you want to start
+            // Like so:
+            // Dialoguer.StartDialogue(0);
+
+            // We'll use the ID method for now, in order to avoid any errors when you start creating your own dialogues.
+            Dialoguer.StartDialogue(0, dialoguerCallback);
+            this.enabled = false; 
+
+            // By default, the DialoguerDialogues enum is automatically updated when you save your dialogues.
+            // You can turn this off in the Dialoguer presferences menu.
+
+        }
+    }//end of OnGUI method
+
+    //callback method
+    private void dialoguerCallback()
+    {
+        this.enabled = true;
+    }
 }
